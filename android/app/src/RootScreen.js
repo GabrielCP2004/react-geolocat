7e96cffe250756dd2b6c8ba50ea687cd3ad84a83
Gabriel da Conceição Pereira <gabriel.con.pere37@outlook.com>
2021-07-14 03:03:31 -0400

    first commit

---
+++ android/app/src/RootScreen.js
@@ -0,0 +1,90 @@
+import React, { useEffect } from 'react'
+import { View, Text } from 'react-native'
+
+import { PermissionsAndroid } from 'react-native'
+import RNLocation from 'react-native-location';
+import ReactNativeForegroundService from '@supersami/rn-foreground-service';
+
+RNLocation.configure({
+  distanceFilter: 100, // Meters
+  desiredAccuracy: {
+    android: 'balancedPowerAccuracy'
+  },
+  // Android only
+  androidProvider: 'auto',
+  interval: 5000, // Milliseconds
+  fastestInterval: 10000, // Milliseconds
+  maxWaitTime: 5000, // Milliseconds
+  // iOS Only
+  // activityType: 'other',
+  // allowsBackgroundLocationUpdates: false,
+  // headingFilter: 1, // Degrees
+  // headingOrientation: 'portrait',
+  // pausesLocationUpdatesAutomatically: false,
+  // showsBackgroundLocationIndicator: false,
+});
+let locationSubscription = null;
+let locationTimeout = null;
+
+export default function RootScreen () {
+
+  useEffect(() => {
+    const requestPremission = async () => {
+      const backgroundgranted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_BACKGROUND_LOCATION,
+        {
+          title: 'Background Location Permission',
+          message:
+            'We need access to your location ' +
+            'so you can get live quality updates.',
+          buttonNeutral: 'Ask Me Later',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK',
+        },
+      );
+      if (backgroundgranted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log('Permission garanted')
+
+        ReactNativeForegroundService.add_task(
+          () => {
+            RNLocation.requestPermission({
+              android: {
+                detail: 'fine',
+              },
+            }).then((granted) => {
+              console.log('Location Permissions: ', granted);
+              // if has permissions try to obtain location with RN location
+              if (granted) {
+                locationSubscription && locationSubscription();
+                locationSubscription = RNLocation.subscribeToLocationUpdates(
+                  ([locations]) => {
+                    locationSubscription();
+                    locationTimeout && clearTimeout(locationTimeout);
+                    console.log(locations);
+                  },
+                );
+              } else {
+                locationSubscription && locationSubscription();
+                locationTimeout && clearTimeout(locationTimeout);
+                console.log('no permissions to obtain location');
+              }
+            });
+          },
+          {
+            delay: 1000,
+            onLoop: true,
+            taskId: 'taskid',
+            onError: (e) => console.log('Error logging:', e),
+          },
+        );
+      }
+    }
+    requestPremission();
+  },[])
+
+  return (
+    <View>
+      <Text>Hello word!</Text>
+    </View>
+  )
+}
--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import 'react-native-gesture-handler';
+import ReactNativeForegroundService from "@supersami/rn-foreground-service";
 import { registerRootComponent } from 'expo';
 
 import App from './App';
 
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
+ReactNativeForegroundService.register();
+ReactNativeForegroundService.start({
+  id: 144,
+  title: "Foreground Service",
+  message: "you are online!",
+});
 registerRootComponent(App);
